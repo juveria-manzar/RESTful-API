9ebc077b9209d676b74dba804bd4ce38a57b8a6c
Juveria Manzar <juveriadot@gmail.com>
2020-05-06 14:31:37 +0530

    HTTP-verbs

--- RESTful-Routing/mvc/controllers/index.js
+++ RESTful-Routing/mvc/controllers/index.js
@@ -1,12 +1,30 @@
-
 getIndex = function(req, res, next) {
-  res.render('index', { title: 'RESTful Routing' });
+    res.render('index', { title: 'RESTful Routing' });
+}
+getExample = function(req, res) {
+    res.render('example', { title: "Example HTTP Requests" })
+}
+getReqExample = function(req, res) {
+    res.json({ message: "User made a GET request" })
+}
+postReqExample = function(req, res) {
+    res.json({ message: "User made a POST request" })
+}
+putReqExample = function(req, res) {
+    res.json({ message: "User made a PUT request" })
+}
+deleteReqExample = function(req, res) {
+    res.json({ message: "User made a DELETE request" })
 }
 
 
 
 
-
 module.exports = {
-    getIndex
+    getIndex,
+    getExample,
+    getReqExample,
+    putReqExample,
+    postReqExample,
+    deleteReqExample
 }
