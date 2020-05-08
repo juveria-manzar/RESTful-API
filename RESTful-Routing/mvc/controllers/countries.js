b2bfb5a6af084c568bc9e21e182b6b27d5880ccb
Juveria Manzar <juveriadot@gmail.com>
2020-05-09 03:14:38 +0530

    using-created-api

--- RESTful-Routing/mvc/controllers/countries.js
+++ RESTful-Routing/mvc/controllers/countries.js
@@ -1,10 +1,40 @@
 const request = require('request');
+let domainPath = "http://localhost:3000"
+
 
 getCountryList = function(req, res, next) {
-    res.render('index', { title: 'RESTful Routing' });
+    const path = "/api/countries";
+    const requestOption = {
+        url: `${domainPath}${path}`,
+        method: 'GET'
+    }
+    request(requestOption, (err, response, body) => {
+        if (err) { return res.json({ error: err }); }
+        let countries = JSON.parse(body).countries
+        console.log((body).countries)
+        if (response.statusCode === 200) {
+            return res.render('countries', { title: "List of Countries", countries: countries })
+        } else {
+            return res.json({ message: "Something went wrong" })
+        }
+    })
 }
-getCountry = function(req, res, next) {
-    res.render('index', { title: 'RESTful Routing' });
+getCountry = function({ params }, res, next) {
+
+    const path = `/api/countries/${params.countryid}`;
+    const requestOption = {
+        url: `${domainPath}${path}`,
+        method: 'GET'
+    }
+    request(requestOption, (err, response, body) => {
+        if (err) { return res.json({ error: err }); }
+        let country = JSON.parse(body).country;
+        if (response.statusCode === 200) {
+            return res.render('country', { title: "List of Countries", country: country })
+        } else {
+            return res.json({ message: "Something went wrong" })
+        }
+    })
 }
 createCountry = function(req, res, next) {
     res.render('index', { title: 'RESTful Routing' });
